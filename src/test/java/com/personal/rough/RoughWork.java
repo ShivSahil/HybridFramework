5fe5b28af901293a2c501e0dad686110dda4023d
ShivSahil <shivguleri@tuta.io>
2021-05-26 09:46:31 +0530

    First commit

---
+++ src/test/java/com/personal/rough/RoughWork.java
@@ -0,0 +1,16 @@
+package com.personal.rough;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class RoughWork {
+
+	@Test
+	public void method1() {
+		   System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		   WebDriver driver=new ChromeDriver();
+
+	}
+
+}
