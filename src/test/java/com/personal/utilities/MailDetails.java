c194dddef36854b6ae5879ac0407724d8335b4a2
Shiv Sahil Guleri <77981090+ShivSahil@users.noreply.github.com>
2021-08-26 15:44:44 +0530

    Update MailDetails.java

--- src/test/java/com/personal/utilities/MailDetails.java
+++ src/test/java/com/personal/utilities/MailDetails.java
@@ -3,7 +3,7 @@ package com.personal.utilities;
 public class MailDetails{
     public static String server="smtp.gmail.com";
     public static String from = "sahilguleri.9@gmail.com";
-    public static String password = "Bran07Broken#";
+    public static String password = "PWD";
     public static String[] to ={"shivguleri@tuta.io"}; // can sent mail to multiple addresses
     public static String subject = " Subject";
     
